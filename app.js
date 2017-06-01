1d51ff06dcb6720ddb5bdf322ad04bfead4204f0
Sam Milechman <sammilechman@gmail.com>
2017-06-01 09:42:35 -0700

    config.js (not checked into version control) set up for config vars

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const config = require('./config');
+const serverPort = config.serverPort;
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+
+app.listen(serverPort, function () {
+  console.log(`server listening on port ${serverPort}!`)
 })
---
+++ config-example.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'serverPort' : 'SECRET'
+}
